bb8b0252195c12c0ab059297dc346068e9e67859
Davi Figueiredo <davifrdutra@gmail.com>
2026-07-05 11:49:46 -0300

    att

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ const translations = {
 }
 
 const i18nElements = document.querySelectorAll("[data-i18n]");
-const lang = "en";
+let lang = "en";
 
-i18nElements.forEach((element) => {
-  const key = element.dataset.i18n;
-  const text = translations[lang][key];
-
-});
+function applyTranslations() {
+  i18nElements.forEach((element) => {
+    const key = element.dataset.i18n;
+    const text = translations[lang][key];
+    element.textContent = text;
+  });
+}
